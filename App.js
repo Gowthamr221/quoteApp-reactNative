ca356d1e482327c7eabbc0cfafa2abb348d62090
Gowthamr221 <gowthamr221@gmail.com>
2022-03-03 18:56:02 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, StyleSheet,Button, Text, View } from 'react-native';
+import {useState} from 'react'
+const list = require('./quotes.json')
+
+
 
 export default function App() {
+  const [quote,setQuote] = useState({author:'Author',quote :'quote'})
+  let data =list["quotes"][Math.ceil(Math.random()*list["quotes"].length-1)]
   return (
+    <View style={{height:'100%'}} >
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{color:"white",padding:25,fontSize:25}}>{data.quote}</Text>
+      <Text style={{color:"white",textAlign:'right',padding:15,fontSize:12}}>- {data.author}</Text>
+     
+    </View>
+      <TouchableOpacity
+        onPress={
+          ()=>{
+            setQuote(list["quotes"][Math.ceil(Math.random()*list["quotes"].length-1)])
+           }
+        }
+    style={{
+      borderWidth: 1,
+      borderColor: 'rgba(0,0,0,0.2)',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      position: 'absolute',
+      bottom: 30,
+      right:'5%',
+      left:'5%',
+      height: 60,
+      width:'90%',
+      backgroundColor: '#fff',
+      borderRadius:100
+    }}
+  >
+  <Text>Get Quote</Text>
+  </TouchableOpacity>
+      <StatusBar style="auto" backgroundColor='white' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    
+    backgroundColor: 'black',
+    padding: 25,
     justifyContent: 'center',
+    height:'100%',
+    
   },
 });
